8cf5365efe034ace5a17806626a3e1bc373117b5
AlisonDaFonseca <alisonfonseca.st@gmail.com>
2023-05-05 02:57:53 -0300

    Edição de nome

--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
 
 export default function User() {
+    let [nome, setNome] = useState('catanacomics');
+
+    function perguntarNome(){
+        let novoNome = prompt('Digite seu nome: ');
+            setNome(novoNome);
+    }
+
     return (
         <div class="usuario">
             <img src="assets/img/catanacomics.svg" alt="imagem de perfil" />
             <div class="texto">
                 <span>
-                    <strong>catanacomics</strong>
-                    <ion-icon name="pencil"></ion-icon>
+                    <strong>{nome ? `${nome}` : 'catanacomics'}</strong>
+                    <ion-icon name="pencil" onClick={perguntarNome}></ion-icon>
                 </span>
             </div>
         </div>
