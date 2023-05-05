3d33f000945134ec6a6da27e61193880a6326ba6
AlisonDaFonseca <alisonfonseca.st@gmail.com>
2023-05-05 03:05:54 -0300

    mudar imagem

--- src/components/User.js
+++ src/components/User.js
@@ -2,15 +2,20 @@ import { useState } from "react";
 
 export default function User() {
     let [nome, setNome] = useState('catanacomics');
+    let [imagem, setImagem] = useState("assets/img/catanacomics.svg");
 
     function perguntarNome(){
         let novoNome = prompt('Digite seu nome: ');
             setNome(novoNome);
     }
+    function perguntarLinkImagem(){
+        let novaImagem = prompt('Digite o link da imagem: ');
+            setImagem(novaImagem);
+    }
 
     return (
         <div class="usuario">
-            <img src="assets/img/catanacomics.svg" alt="imagem de perfil" />
+            <img src={imagem ? `${imagem}` : "assets/img/catanacomics.svg"} alt="imagem de perfil" onClick={perguntarLinkImagem}/>
             <div class="texto">
                 <span>
                     <strong>{nome ? `${nome}` : 'catanacomics'}</strong>
